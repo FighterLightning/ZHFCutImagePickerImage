15fb6f98c5c409ba6e1a3af195b1cfabbd420a79
ZhangHaifeng <18500473583@163.com>
2017-09-20 21:47:16 +0800

    初始化项目

---
+++ ZHFCutImagePickerImage/ZHFCutImagePickerImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZHFCutImagePickerImage
+//
+//  Created by 张海峰 on 2017/9/20.
+//  Copyright © 2017年 张海峰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
